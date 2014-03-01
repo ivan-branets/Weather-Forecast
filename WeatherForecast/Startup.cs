b2c619387cad4975e2e39aa0e407a7cd35c07f84
ice <ivan.branets@gmail.com>
2014-03-01 15:03:27 +0200

    updated libs

---
+++ WeatherForecast/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WeatherForecast.Startup))]
+
+namespace WeatherForecast
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
